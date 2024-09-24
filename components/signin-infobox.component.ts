c20995ec0680f162050e1841a3f1e87b74227dbf
Filip Rozenek <60395699+filiprozenek@users.noreply.github.com>
2024-09-24 17:00:59 +0200

    Delete components directory

--- components/signin-infobox.component.ts
+++
@@ -1,7 +0,0 @@
-import { Page } from "@playwright/test";
-
-export class SigninInfoBox {
-    constructor(private page: Page) {}
-
-    signinInfoBoxUserNameTitle = this.page.getByRole('heading', { name: 'Accepted usernames are:' });
-}
